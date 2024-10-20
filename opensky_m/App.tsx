b28cc65f8098a91525cad6bbfe5b99679d4ed788
Baddewithana P <99246737+IT21247804@users.noreply.github.com>
2024-10-21 01:50:36 +0530

    error(android build): build not completed. try to change package.json

--- opensky_m/App.tsx
+++ opensky_m/App.tsx
@@ -1,20 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DrawerLayout from '@/app/_layout';
+import { registerRootComponent } from 'expo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+registerRootComponent(DrawerLayout);
